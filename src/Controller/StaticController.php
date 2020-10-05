9a6a5551a9045a19018e5bd89814d135e7532775
Mairesse Valentin <mairesse.valentin.pro@gmail.com>
2020-10-05 11:18:03 +0000

    mail system working

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -21,13 +21,22 @@ class StaticController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request){
+    public function contact(Request $request, \Swift_Mailer $mailer){
         $form = $this->createForm(ContactType::class);
 
         if($request->isMethod('POST')){
             $form->handleRequest($request);
             if($form->isSubmitted() && $form->isValid()){
-                $this->addFlash('notice','Bouton appuyé');
+                $this->addFlash('notice','Message envoyé !');
+                
+                // Envoi de l'email
+                $message = (new \Swift_Message($form->get('subject')->getData()))
+                    ->setFrom($form->get('email')->getData())
+                    ->setTo('mairesse.valentin.pro@gmail.com')
+                    ->setBody($this->renderView('email/emails.html.twig', array('name'=>$form->get('name')->getData(), 'subject'=>$form->get('subject')->getData(), 'message'=>$form->get('message')->getData())), 'text/html');
+
+                $mailer->send($message);
+                return $this->redirectToRoute('contact');
             }
         }
 
